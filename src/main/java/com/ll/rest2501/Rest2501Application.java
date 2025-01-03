12c074bbc34ab02837e78771b95217d6a879f80d
sik2 <bickwang123@gmail.com>
2025-01-03 14:18:30 +0900

    init

---
+++ src/main/java/com/ll/rest2501/Rest2501Application.java
@@ -0,0 +1,13 @@
+package com.ll.rest2501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest2501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest2501Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/rest2501/Rest2501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.rest2501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest2501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
